1115cab3e23b65e98d0cf1040f22c3fd056c728b
sandeep169 <48309774+sandeep169@users.noreply.github.com>
2021-05-22 00:39:24 +0530

    redux added on HomeScreen

---
+++ frontend/src/actions/productActions.js
@@ -0,0 +1,16 @@
+import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstants"
+import Axios from 'axios';
+export const listProducts = () => async (dispatch) => {
+    dispatch({
+        type: PRODUCT_LIST_REQUEST
+    });
+    try {
+        const { data } = await Axios.get('/api/products');
+        dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
+        //payload should contain from backend    
+    }
+    catch (error) {
+        dispatch({ type: PRODUCT_LIST_FAIL });
+    };
+
+};
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,34 +1,44 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, } from 'react';
+//useState used for static data state : import React, { useEffect, useState } from 'react';
 import Product from '../components/Product';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
-
-import axios from 'axios';
+import {useDispatch, useSelector} from 'react-redux';
+import { listProducts } from '../actions/productActions';
+// import axios from 'axios'; //used for static state
 // import data from '../data';
 // console.log(axios);
 export default function HomeScreen() {
+    const dispatch = useDispatch(); //hook in  react-redux
     // console.log(axios);
+    //no need now 
+    // const [products, setProducts] = useState([]);
+    // const [loading, setLoading] = useState(false);
+    // const [error, setError] = useState(false)
+    const productList = useSelector(state=>state.productList);
+    const {loading,error,products} = productList;
 
-    const [products, setProducts] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false)
-
+    //three value from PL .. array destructuring.. unpacking value from array
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                setLoading(true);
-                const { data } = await axios.get('/api/products'); //obj will from axios ,this will return  data 
-                setLoading(false);
-                setProducts(data);
-            }catch (err) {
-                setError(err.message);
-                setLoading(false);
-            }
-        };
+        //dispatch an action which we gonna listProducts,its a function
+        dispatch(listProducts());
 
-        fetchData();
+        //used in static state data
+        // const fetchData = async () => {
+        //     try {
+        //         setLoading(true);
+        //         const { data } = await axios.get('/api/products'); //obj will from axios ,this will return  data 
+        //         setLoading(false);
+        //         setProducts(data);
+        //     }catch (err) {
+        //         setError(err.message);
+        //         setLoading(false);
+        //     }
+        // };
 
-    }, []);
+        // fetchData();
+
+    }, [dispatch]);
 
 
 
