06108b9921ba2f1a2f650d506ca60c5aee2d4244
sandeep169 <48309774+sandeep169@users.noreply.github.com>
2021-05-26 04:04:26 +0530

    product screen api created

--- backend/server.js
+++ backend/server.js
@@ -4,22 +4,22 @@ import data from './data.js'
 
 // const data =require('./data')
 //create an app
-const app=express(); 
+const app = express();
 
-app.get('/',(req,res) => {
+app.get('/', (req, res) => {
     res.send("server is ready");
 });
-
+//product screen api
 app.get('/api/products/:id', (req, res) => {
     const product = data.products.find((x) => x._id === Number(req.params.id));
     if (product) {
-      res.send(product);
+        res.send(product);
     } else {
-      res.status(404).send({ message: 'Product Not Found' });
+        res.status(404).send({ message: 'Product Not Found' });
     }
-  });
+});
 
-app.get('/api/products',(req,res)=>{
+app.get('/api/products', (req, res) => {
     res.send(data.products);
     // console.log(data.products);
 });
@@ -27,6 +27,6 @@ app.get('/api/products',(req,res)=>{
 //     console.log('serve at http://localhost:5000');
 // });
 const port = process.env.PORT || 5000;
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`server running at http://localhost:${port}`);
 })
