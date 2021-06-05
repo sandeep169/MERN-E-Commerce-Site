a269e3aebb9165f35935e93c4306bdaa5da1bb08
sandeep169 <48309774+sandeep169@users.noreply.github.com>
2021-06-05 13:41:29 +0530

    contact and register page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,10 @@ import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import { useSelector } from 'react-redux';
+import { Contact } from './Contact';
+import { Footer } from './Footer';
+import Register from './Register';
+
 
 // import Rating from './components/Rating';
 
@@ -21,28 +25,31 @@ const  App=()=> {
                         <Link className="brand" to="/">Klieder House</Link>
                     </div>
                     <div>
+                    <Link to="/products">products</Link>
                         {/* <!-- links --> */}
+
                         <Link to="/cart">Cart
                         {cartItems.length > 0 && (
                             <span className="badge">{cartItems.length}</span>
                         )}
                         </Link>
-                        <Link to="/signin">Sign Up</Link>
+                        <Link to="/contact">Contact</Link>
+                        <Link to="/Register">Sign Up</Link>
                     </div>
                 </header>
                 {/* <!-- image size 680px by 830 px --> */}
                 {/* <!-- body of card ,, product body  --> */}
                 <main>
                 <Route path="/cart/:id?" component={CartScreen}></Route>
-                
                 <Route path="/product/:id" component={ProductScreen}></Route>
-                <Route path="/" component={HomeScreen} exact></Route>
-                
-                
-                </main>
+                <Route path="/products" component={HomeScreen} exact></Route>
+                <Route path="/contact" component={Contact}></Route>
+                <Route path="/Register" component={Register}></Route>
 
+                </main>
                 <footer className="row center">
-                    All right reserved to Klieder House.
+                    {/* All right reserved to Klieder House. */}
+                            <Footer />
         </footer>
             </div>
             </BrowserRouter>
