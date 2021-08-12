5f914b661abd1b2347c8b068d6e6fa1f90b2b9a2
arihant2 <arihantjain136@gmail.com>
2021-08-12 18:05:26 +0530

    added functionality to show all products on home pg

--- backend/controllers/home.js
+++ backend/controllers/home.js
@@ -1,15 +1,25 @@
-export const getAdminHomeController = (req, res) => {
-    const role = req.user ? req.user.group : req.userRole;
-    res.send(`yoo..its admin!\nrole: ${role}`);
-};
+import productModel from '../models/product.js';
 
-export const getUserHomeController = (req, res) => {
-    const role = req.user ? req.user.group : req.userRole;
-    res.send(`yoo..its user!\nrole: ${role}`);
-};
+// import { tryCatchUtility } from '../utils/errHandling/tryCatch.js';
+// import { generateErrUtility } from '../utils/errHandling/generateErr.js';
 
-export const getGuestHomeController = (req, res) => {
-    const role = req.userRole || 'guest';
-    res.send(`yoo..its guest!<br>role: ${role}`);
-};
+// const common = tryCatchUtility(async (req, res) => {
+async function common(req, res) {
+    // try {
+        const response = await productModel.find().lean(); // .catch // .then(_, err => { console.log(err); });
+        res.status(200).json({
+            user: (req.user ? req.user.group : req.userRole) || 'guest',
+            products: response.length ? response : 'No product available!'
+        });
+    // } catch (err) {
+    //     console.log(err);
+    //     // throw new generateErrUtility(err);
+    // }
+}
+
+export const getAdminHomeController = common;
+
+export const getUserHomeController = common;
+
+export const getGuestHomeController = common;
 
